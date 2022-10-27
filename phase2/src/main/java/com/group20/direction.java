8a92c719f769f53d70022d3232cd79ea4364746f
eca104 <eca104@sfu.ca>
2022-10-27 00:38:43 -0700

    implement direction enum

---
+++ phase2/src/main/java/com/group20/direction.java
@@ -0,0 +1,4 @@
+package com.group20;
+public enum direction {
+    up,down,left,right,pause;
+}
--- phase2/src/main/java/com/group20/keyListener.java
+++ phase2/src/main/java/com/group20/keyListener.java
@@ -5,6 +5,30 @@ import java.awt.event.*;
 
 
 public class keyListener implements KeyListener {
+
+    int up,down,right,left=0;
+    
+   
+    public direction Direction(){
+        int add=(up+down+right+left);
+        if(add>1 || add==0 ){
+            return direction.pause;
+        }
+        else if(up==1){
+            return direction.up;
+        }
+        else if(down==1){
+            return direction.down;
+        }
+        else if(left==1){
+            return direction.left;
+        }
+        else {
+            return direction.right;
+        }
+        
+    }
+
     @Override
     public void keyTyped(KeyEvent e){
         
@@ -12,21 +36,38 @@ public class keyListener implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e){
-        //keyPressed = Invoked when a physical key is pressed down. Uses KeyCode, int output
+        
 		switch(e.getKeyCode()) {
             case 37: 
+                left=1;
                 break;
             case 38: 
+                down=1;
                 break;
             case 39: 
+                up=1;
                 break;
             case 40:
+                right=1;
                 break;
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e){
-        
+        switch(e.getKeyCode()) {
+            case 37: 
+                left=1;
+                break;
+            case 38: 
+                down=1;
+                break;
+            case 39: 
+                up=1;
+                break;
+            case 40:
+                right=1;
+                break;
+        }
     }
 }
