062901e4954bcd1a102fac3134acda38fa7bda40
eca104 <eca104@sfu.ca>
2022-10-27 12:49:35 -0700

    update keylistener

--- phase2/src/main/java/com/group20/keyListener.java
+++ phase2/src/main/java/com/group20/keyListener.java
@@ -6,29 +6,11 @@ import java.awt.event.*;
 
 public class keyListener implements KeyListener {
 
-    int up,down,right,left=0;
-    
-   
-    public direction Direction(){
-        int add=(up+down+right+left);
-        if(add>1 || add==0 ){
-            return direction.pause;
-        }
-        else if(up==1){
-            return direction.up;
-        }
-        else if(down==1){
-            return direction.down;
-        }
-        else if(left==1){
-            return direction.left;
-        }
-        else {
-            return direction.right;
-        }
-        
-    }
+   private eventManager event;
 
+   public keyListener(eventManager event){
+    this.event=event;
+   }
     @Override
     public void keyTyped(KeyEvent e){
         
@@ -39,16 +21,17 @@ public class keyListener implements KeyListener {
         
 		switch(e.getKeyCode()) {
             case 37: 
-                left=1;
+            System.out.print("up");
+                event.left=1;
                 break;
             case 38: 
-                down=1;
+                event.down=1;
                 break;
             case 39: 
-                up=1;
+                event.up=1;
                 break;
             case 40:
-                right=1;
+                event.right=1;
                 break;
         }
     }
@@ -57,16 +40,16 @@ public class keyListener implements KeyListener {
     public void keyReleased(KeyEvent e){
         switch(e.getKeyCode()) {
             case 37: 
-                left=1;
+                event.left=0;
                 break;
             case 38: 
-                down=1;
+                event.down=0;
                 break;
             case 39: 
-                up=1;
+                event.up=0;
                 break;
             case 40:
-                right=1;
+                event.right=0;
                 break;
         }
     }
