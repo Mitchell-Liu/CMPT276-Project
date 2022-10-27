209f6b7355c743cedc06716a9206c30bd81654df
eca104 <eca104@sfu.ca>
2022-10-27 12:49:45 -0700

    update main

--- phase2/src/main/java/com/group20/Main.java
+++ phase2/src/main/java/com/group20/Main.java
@@ -1,11 +1,24 @@
  package com.group20;
  
-
+ import java.util.Timer;
+ import java.util.TimerTask;
 
  public class Main {
    public static void main(String[] argv) throws Exception {
     new Frame();
     
+    Timer t = new Timer();  
+    TimerTask tt = new TimerTask() {  
+    @Override  
+    public void run() {  
+        System.out.println("Task Timer on Fixed Rate");  
+    };  
+  };  
+  t.scheduleAtFixedRate(tt,500,1000); 
+
+
+
+
  }}
 
  
