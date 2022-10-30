04384e0a52f6b5f844d3d08fb0862cbef3f545d0
eca104 <eca104@sfu.ca>
2022-10-29 17:11:37 -0700

    combine keylistener to eventmanager

--- phase2/src/main/java/com/group20/eventManager.java
+++ phase2/src/main/java/com/group20/eventManager.java
@@ -1,21 +1,93 @@
 package com.group20;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+public class eventManager implements KeyListener{
+    
+	
+	
+
+	
 
 
-public class eventManager {
-    static int up,down,right,left=0;
-    private eventManager event=new eventManager();
-    eventManager(){
-        new keyListener(event);
 
+
+     
+     int up,down,right,left;
+     JTextArea ta;
+    //private eventManager event=new eventManager();
+    public eventManager(){
+       
+        
+         up=0;
+         down=0;
+         right=0;
+         left=0;
 
 
     }
 
     
+    @Override
+    public void keyTyped(KeyEvent e){
+        
+    }
 
+    @Override
+    public void keyPressed(KeyEvent e){
+       
+		switch(e.getKeyCode()) {
+            case 37: 
+           
+                left=1;
+                //System.out.print("left");
+                break;
+            case 38: 
+            
+                up=1;
+                //System.out.print("up");
+                break;
+            case 39: 
+            
+                right=1;
+                //System.out.print("right");
+                break;
+            case 40:
+           
+                down=1;
+                //System.out.print("down");
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e){
+        switch(e.getKeyCode()) {
+            case 37: 
+           
+                left=0;
+                //System.out.print("left");
+                break;
+            case 38: 
+            
+                up=0;
+                //System.out.print("up");
+                break;
+            case 39: 
+            
+                right=0;
+                //System.out.print("right");
+                break;
+            case 40:
+           
+                down=0;
+                //System.out.print("down");
+                break;
+        }
+    }
 
     //each tick, event manager will call this function to get the direction
-     public static direction Direction(){
+     public direction Direction(){
          int add=(up+down+right+left);
          if(add>1 || add==0 ){
              
@@ -35,6 +107,7 @@ public class eventManager {
          }
          
      }
+     
 
 
 }
