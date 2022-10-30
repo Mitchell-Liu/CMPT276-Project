847073682ae076e1b74699a411a47742c888fdb8
eca104 <eca104@sfu.ca>
2022-10-29 17:11:55 -0700

    more testing code for timer

--- phase2/src/main/java/com/group20/Main.java
+++ phase2/src/main/java/com/group20/Main.java
@@ -2,16 +2,50 @@
  
  import java.util.Timer;
  import java.util.TimerTask;
-
+ import java.util.concurrent.TimeUnit;
  public class Main {
    public static void main(String[] argv) throws Exception {
-    new StartMenu();
-    
+    StartMenu menu=new StartMenu();
+    eventManager event=new eventManager();
+    menu.addKeyListener(event);
+    //this.addKeyListener(new eventManager());
+    while(true){
+      TimeUnit.SECONDS.sleep(1);
+      direction di=event.Direction();
+      
+     
+      switch(di){
+        case up:
+        System.out.println("up"); 
+        break;
+        case down:
+        System.out.println("down"); 
+        break;
+        case right:
+        System.out.println("right"); 
+        break;
+        case left:
+        System.out.println("left"); 
+        break;
+        case pause:
+        System.out.println("pause"); 
+        break;
+        
+      }
+    }
+     /* 
     Timer t = new Timer();  
     TimerTask tt = new TimerTask() {  
+
+
+
+   
     @Override  
     public void run() {  
-        direction di=eventManager.Direction();
+      
+      direction di=event.Direction();
+      
+     
         switch(di){
           case up:
           System.out.println("up"); 
@@ -30,6 +64,7 @@
           break;
           
         }
+        
          
     };  
   };  
@@ -38,6 +73,26 @@
 
 
 
- }}
+
+
+
+
+*/
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+}
 
  
