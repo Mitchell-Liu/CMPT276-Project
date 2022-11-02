08c4c3a2cfbc1fac4a7ddec79efb6c860934671b
eca104 <eca104@sfu.ca>
2022-11-01 20:17:17 -0700

    implement timer

---
+++ phase2/src/main/java/com/group20/Timer.java
@@ -0,0 +1,52 @@
+package com.group20;
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
+public class Timer extends JLabel  implements Runnable{
+    int time=0;
+    boolean running;
+    boolean exit=false;
+    Timer(){
+        super("0 s");
+       // this.event=event;
+        running=true;
+        this.setForeground(Color.yellow);
+        this.setOpaque(false);
+        setFont(new Font("Serif", Font.PLAIN, 30));
+        
+        Thread thread = new Thread(this);
+        thread.start();
+        
+    }
+
+    public void run(){
+        while(!exit){
+
+        if(running){
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000);
+                //Thread.sleep(2000);
+            } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+            }
+            //System.out.println(time);
+            this.setText(Integer.toString(time)+ " s");
+            time++;
+       }
+        
+        }
+    }
+
+    public void pause(){
+        running=false;
+    }
+    public void resume(){
+        running=true;
+    }
+
+    public void exit(){
+        exit=true;
+    }
+    eventManager event;
+}
