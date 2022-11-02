ba21436a9f91ee0556ad2a802aeeb2f0b842b379
eca104 <eca104@sfu.ca>
2022-11-01 20:16:54 -0700

    update main

--- phase2/src/main/java/com/group20/Main.java
+++ phase2/src/main/java/com/group20/Main.java
@@ -4,10 +4,9 @@
  public class Main {
    public static void main(String[] argv) throws Exception {
     StartMenu menu=new StartMenu();
-    eventManager event=new eventManager();
-    menu.addKeyListener(event);
-    Tick tick=new Tick(event);
-    tick.run();
+    
+    //tick.run();
+    
    
 
 
