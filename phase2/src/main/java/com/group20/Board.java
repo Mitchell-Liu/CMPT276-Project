168807013c1c2acb1f6493f5a4c43dd934386246
eca104 <eca104@sfu.ca>
2022-11-03 13:44:15 -0700

    change file path search current directory

--- phase2/src/main/java/com/group20/Board.java
+++ phase2/src/main/java/com/group20/Board.java
@@ -25,8 +25,8 @@ public class Board extends JPanel{
         map = gameMap;
 
         board = new JLabel[20][20];
-
-        File path = new File(System.getProperty("user.dir")+"/project/phase2/src/main/java/com/group20/Assets");
+        
+        File path = new File("./phase2/src/main/java/com/group20/Assets");
 
         File[] allFiles = path.listFiles();
 
