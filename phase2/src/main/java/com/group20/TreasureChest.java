5bd79553315cbd05d7138906ee094629433b2b9d
eca104 <eca104@sfu.ca>
2022-11-06 21:13:39 -0800

    add more comments

--- phase2/src/main/java/com/group20/TreasureChest.java
+++ phase2/src/main/java/com/group20/TreasureChest.java
@@ -1,5 +1,7 @@
 package com.group20;
-
+/**
+ * TreasureChest for the player to collect
+ */
 public class TreasureChest extends Collectible{
     TreasureChest(Position pos){
         this.setValue(20);
