1e06fc2e37c87a6db9bdf108f972570aa88fb965
pxa4 <pxa4@sfu.ca>
2022-11-21 18:54:48 -0800

    Added a couple more tests to MapTest

--- phase2/src/test/java/com/group20/MapTest.java
+++ phase2/src/test/java/com/group20/MapTest.java
@@ -20,6 +20,56 @@ public class MapTest {
         map.setDiver(player);
         map.setExit(exit);
     }
+
+    //Testing position validation function using edge/boundary cases
+    @Test
+    public void testValidPositions(){
+        Position pos1 = new Position(0, 0);
+        Position pos2 = new Position(19, 19);
+        Position pos3 = new Position(0, 19);
+        Position pos4 = new Position(19, 0);
+        Position pos5 = new Position(10, 10);
+        Position pos6 = new Position(20, 0);
+        Position pos7 = new Position(0, 20);
+        Position pos8 = new Position(20, 20);
+        Position pos9 = new Position(-1, 0);
+        Position pos10 = new Position(0, -1);
+        Position pos11 = new Position(-1, -1);
+        assertTrue(map.positionisValid(pos1));
+        assertTrue(map.positionisValid(pos2));
+        assertTrue(map.positionisValid(pos3));
+        assertTrue(map.positionisValid(pos4));
+        assertTrue(map.positionisValid(pos5));
+        assertFalse(map.positionisValid(pos6));
+        assertFalse(map.positionisValid(pos7));
+        assertFalse(map.positionisValid(pos8));
+        assertFalse(map.positionisValid(pos9));
+        assertFalse(map.positionisValid(pos10));
+        assertFalse(map.positionisValid(pos11));
+    }
+
+    //Tests that proper names are retrieved by getEntityNameAt function
+    @Test
+    public void testEntityNames(){
+        Position pos1 = new Position(0, 0);
+        Position pos2 = new Position(1, 0);
+        Position pos3 = new Position(2, 0);
+        Position pos4 = new Position(3, 0);
+        Position pos5 = new Position(4, 0);
+        map.placeCoin(pos1);
+        map.placeSeaweed(pos2);
+        map.placeTreasureChest(pos3);
+        map.placeWall(pos4);
+        map.setSharks(new Shark(pos5));
+        map.openExit();
+        assertEquals(map.getEntityNameAt(pos1), "Coin");
+        assertEquals(map.getEntityNameAt(pos2), "Seaweed");
+        assertEquals(map.getEntityNameAt(pos3), "TreasureChest");
+        assertEquals(map.getEntityNameAt(pos4), "Wall");
+        assertEquals(map.getEntityNameAt(pos5), "Shark");
+        assertEquals(map.getEntityNameAt(map.getDiverPosition()), "Diver");
+        assertEquals(map.getEntityNameAt(new Position(19, 19)), "Exit");
+    }
     
     //Tests behaviour of map when diver moves into walls from all 4 directions
     //Expect diver position to not change if move into wall is attempted
