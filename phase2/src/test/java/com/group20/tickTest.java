7e3b0171d57a348d4d7054cbda2b076d7799deb4
eca104 <eca104@sfu.ca>
2022-11-22 16:45:31 -0800

    tickTest

--- phase2/src/test/java/com/group20/tickTest.java
+++ phase2/src/test/java/com/group20/tickTest.java
@@ -13,11 +13,11 @@ public class tickTest {
     public void testGoesUp(){
         Map map=new Map();
         map.setBorderWalls();
-        Position playerPos = new Position(5, 4);
+        Position playerPos = new Position(5, 5);
         Diver player = new Diver(playerPos);
         map.setDiver(player);
 
-        Position WallPos = new Position(5, 2);
+        Position WallPos = new Position(5, 3);
         map.placeWall(WallPos);
 
         eventManager event=new eventManager(map, null, null, null);
@@ -32,12 +32,42 @@ public class tickTest {
             throw new IllegalStateException(e);
         }
 
-        Position expected=new Position(5,3);
+        Position expected=new Position(5,4);
         Position result=map.getDiverPosition();
-        assertEquals(expected,result);
-
+       
+        assertEquals(expected.getX(),result.getX());
+        assertEquals(expected.getY(),result.getY());
         event.up=0;
     }
 
+    @Test
+    public void testDontGo(){
+        Map map=new Map();
+        map.setBorderWalls();
+        Position playerPos = new Position(5, 5);
+        Diver player = new Diver(playerPos);
+        map.setDiver(player);
 
+        Position WallPos = new Position(5, 3);
+        map.placeWall(WallPos);
+
+        eventManager event=new eventManager(map, null, null, null);
+        Tick tick=new Tick(event);
+
+        event.up=1;
+        event.down=1;
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+
+        Position expected=new Position(5,5);
+        Position result=map.getDiverPosition();
+       
+        assertEquals(expected.getX(),result.getX());
+        assertEquals(expected.getY(),result.getY());
+        event.up=0;
+    }
 }
