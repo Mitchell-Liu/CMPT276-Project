9870f0b25ab3416edccd56b9e1f58929beb7b52d
Adam Watson <adam.watson@shaw.ca>
2022-12-08 18:45:59 -0800

    Additional comments in EndMenu.java

--- phase2/src/main/java/com/group20/EndMenu.java
+++ phase2/src/main/java/com/group20/EndMenu.java
@@ -18,30 +18,30 @@ public class EndMenu extends JFrame{
 	EndMenu(int plScore, boolean result){		
         // Setup the Window
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(1000,800);
+		this.setSize(1000,800); //Set window size to 1000x800 pixels
 		this.setLayout(null);
 		
         // Creates and fits a background for the window
 		ImageIcon backGround = new ImageIcon();
         try{
-            if(result == true){
-                backGround = new ImageIcon(ImageIO.read(this.getClass().getResource("/WinBac.png")));
+            if(result == true){ //if the player wins
+                backGround = new ImageIcon(ImageIO.read(this.getClass().getResource("/WinBac.png"))); //display the win screen
             }
-            else{
-                backGround = new ImageIcon(ImageIO.read(this.getClass().getResource("/LoseBac.png")));
+            else{ //if the player loses
+                backGround = new ImageIcon(ImageIO.read(this.getClass().getResource("/LoseBac.png"))); //display the lose screen
             }
         }
         catch(IOException e){
             e.printStackTrace();
         }
         Image image = backGround.getImage();
-        Image scaledImage = image.getScaledInstance(1000,800 , java.awt.Image.SCALE_SMOOTH);
+        Image scaledImage = image.getScaledInstance(1000,800 , java.awt.Image.SCALE_SMOOTH); //rescale the background image
         backGround = new ImageIcon(scaledImage);
         this.setContentPane((new JLabel(backGround)));
     
         
-		JPanel buttonsC = new JPanel();
         // Stacks the Buttons vertically
+		JPanel buttonsC = new JPanel();
         buttonsC.setLayout(new BoxLayout(buttonsC,BoxLayout.Y_AXIS));
 
         // Create elements to be added to the JFrame
@@ -49,15 +49,17 @@ public class EndMenu extends JFrame{
         ClickButton restartButton = new ClickButton("Restart");
         ClickButton exitButton = new ClickButton("Exit Game");
 
+        // Adds the player's score to the display
         JLabel score = new JLabel("SCORE: " + plScore);
         score.setFont((new Font("Serif", Font.PLAIN, 50)));
         score.setForeground(Color.black);
         
+        // Adds the time elapsed to the display
         JLabel time = new JLabel("TIME: " + Timer.time);
         time.setFont((new Font("Serif", Font.PLAIN, 50)));
         time.setForeground(Color.black);
 
-        // Centers the Buttons
+        // Centers the buttons, the time and the score
 		startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         restartButton.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -65,30 +67,30 @@ public class EndMenu extends JFrame{
         time.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         // Opens the appropriate windows when buttons are clicked
-		startButton.addActionListener(new ActionListener() {
+		startButton.addActionListener(new ActionListener() { //Go to the start menu
             @Override
             public void actionPerformed(ActionEvent e) {
-				new StartMenu();
+				new StartMenu(); //go to the start menu
                 dispose();
             }
         });
-        exitButton.addActionListener(new ActionListener() {
+        exitButton.addActionListener(new ActionListener() { //Exit game
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose();
-				System.exit(0);
+				System.exit(0); //quit the game
             }
         });
-        restartButton.addActionListener(new ActionListener() {
+        restartButton.addActionListener(new ActionListener() { //Restart the game
             @Override
             public void actionPerformed(ActionEvent e) {
                 DefaultGameFactory factory1 = new DefaultGameFactory();
-                new GameInstance(factory1);
+                new GameInstance(factory1); //remake the game level
                 dispose();
             }
         });
         
-        // Adds the buttons the the JFrame
+        // Adds the buttons, score and time to the JFrame
         buttonsC.add(score);
         buttonsC.add(time);
 		buttonsC.add(startButton);
@@ -104,7 +106,7 @@ public class EndMenu extends JFrame{
 		getContentPane().add((buttonsC));
         this.getContentPane().setLayout(null);
 
-		setVisible(true);
+		setVisible(true); //make this window visible
 	}
 
 }
